8be2f91d4b3ddc8cd76575c38ac44b19b9f123d3
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-05-18 03:41:26 +0800

    Add use client to EventList

--- app/events/EventList.tsx
+++ app/events/EventList.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { MouseEvent, useEffect, useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { AnimatePresence, motion } from "framer-motion";

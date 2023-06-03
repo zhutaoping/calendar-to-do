dc48124f4980c862e87af8162d3355788bd3296f
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-06-04 03:51:55 +0800

    Clean up EventForm

--- app/events/EventForm.tsx
+++ app/events/EventForm.tsx
@@ -5,13 +5,6 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Event } from '@prisma/client'
 import { useDay } from '../stores/DayContext'
 import SubmitButton from '../components/SubmitButton'
-import { useEffect, useState } from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Event } from '@prisma/client'
-import { useDay } from '../stores/DayContext'
-import SubmitButton from '../components/SubmitButton'
 
 const schema = z
   .object({

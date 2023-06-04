f29445a50f2d8abe306b63254f5c7a2957e5463b
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-06-04 20:27:49 +0800

    Add newEvent.id to useCreateEvent

--- app/events/hooks/useCreateEvent.ts
+++ app/events/hooks/useCreateEvent.ts
@@ -17,7 +17,7 @@ export const useCreateEvent = () => {
     //* Optimistic update
     //* newEvent is the data that we are sending to the server
     onMutate: async newEvent => {
-      await queryClient.cancelQueries({ queryKey: ['events'] })
+      await queryClient.cancelQueries({ queryKey: ['events', newEvent.id] })
 
       const previousEvents = queryClient.getQueryData<Event[]>(['events']) || []
 

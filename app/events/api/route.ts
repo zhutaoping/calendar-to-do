2175d52cedc22a29e71260155c6ebabfe7df3640
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-06-04 22:01:46 +0800

    For hosting

--- app/events/api/route.ts
+++ app/events/api/route.ts
@@ -1,44 +1,44 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/app/lib/prisma";
-import { Event } from "@prisma/client";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { NextResponse } from 'next/server'
+import { prisma } from '@/app/lib/prisma'
+import { Event } from '@prisma/client'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
 
 export async function GET(req: Request, res: Response) {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
 
-  let events: Event[] = [];
+  let events: Event[] = []
   if (!session) {
     events = await prisma.event.findMany({
       where: {
         userId: null,
       },
-    });
+    })
   } else {
     events = await prisma.event.findMany({
       where: {
         userId: session.user.id,
       },
-    });
+    })
   }
-  return NextResponse.json(events);
+  return NextResponse.json(events)
 }
 
 export async function POST(req: Request, res: Response) {
-  const event: Event = await req.json();
-  console.log("🚀 ~ POST ~ event:", event);
+  const event: Event = await req.json()
+  // console.log("🚀 ~ POST ~ event:", event);
 
   const newEvent = await prisma.event.create({
     data: {
       ...event,
     },
-  });
+  })
 
-  return NextResponse.json(newEvent);
+  return NextResponse.json(newEvent)
 }
 
 export async function PATCH(req: Request, res: Response) {
-  const event: Event = await req.json();
+  const event: Event = await req.json()
 
   const updatedEvent = await prisma.event.update({
     where: {
@@ -50,7 +50,7 @@ export async function PATCH(req: Request, res: Response) {
       endTime: event.endTime,
       completed: event.completed,
     },
-  });
+  })
 
-  return NextResponse.json(updatedEvent);
+  return NextResponse.json(updatedEvent)
 }
